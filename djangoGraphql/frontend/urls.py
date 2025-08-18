7e3fba870e636e5e74cc56bdd0691c68579e7095
hosseinyn <143272670+hosseinyn@users.noreply.github.com>
2025-08-18 18:26:52 +0330

    Delete django-based frontend

--- djangoGraphql/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import list_employees
-
-urlpatterns = [
-    path("" , view=list_employees , name="List Employees"),
-]
--- djangoGraphql/frontend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from employees.models import Employee
-from .forms import EmployeeForm
-
-# Create your views here.
-def list_employees(request):
-    all_employees = Employee.objects.all()
-    return render(request , "list.html" , {"employees" : all_employees})
