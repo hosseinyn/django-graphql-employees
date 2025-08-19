4639fe2cc685c59026d382c92f535c6b77955161
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-19 14:07:23 +0330

    Add create employee feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 
 import List from './pages/List';
+import Add from './pages/Add';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<List />} />
+        <Route path='/add-employee' element={<Add />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Add.jsx
@@ -0,0 +1,89 @@
+import "../assets/styles/Add-styles.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Add = () => {
+    const [formData , setFormData] = useState({
+        firstname : '',
+        lastname : '',
+        age : 0,
+        hireDate : ''
+    });
+    const navigate = useNavigate()
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormData({
+        ...formData,
+        [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        console.log(formData)
+
+        const schema_query = `
+            mutation {
+                createEmployee(firstname:"${formData.firstname}" , lastname:"${formData.lastname}" , age:${formData.age} , hireDate:"${formData.hireDate}"){
+                    employee {
+                        firstname
+                        lastname
+                        age
+                        hireDate
+                        }
+                    }
+            }
+  `
+
+        try {
+            let response = await axios.post("http://127.0.0.1:8000/graphql" , {
+                query : schema_query
+            })
+
+            if (response.data.data.createEmployee) {
+                navigate("/")
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+  return (
+    <>
+      <h1 className="text-center mt-5">Add Employee</h1>
+
+      <center>
+        <form
+          method="post"
+          className="add-employee-form d-flex flex-column mt-5 gap-3 align-items-center"
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="firstname">Firstname : </label>
+          <input type="text" id="firstname" name="firstname" maxLength={50} required placeholder="Enter firstname" onChange={handleChange} />
+
+          <label htmlFor="lastname">Lastname : </label>
+          <input type="text" id="lastname" name="lastname" maxLength={50} required placeholder="Enter lastname" onChange={handleChange} />
+
+          <label htmlFor="age">Age : </label>
+          <input type="number" id="lastname" name="age" max={70} required placeholder="Enter age" onChange={handleChange} />
+
+          <label htmlFor="hireDate">Hire date : </label>
+          <input type="date" id="hireDate" name="hireDate" required placeholder="Enter hire date" onChange={handleChange}  />
+
+          <button className="btn btn-success" type="submit">
+            Add
+          </button>
+        </form>
+      </center>
+
+      <br></br>
+    </>
+  );
+};
+
+export default Add;
